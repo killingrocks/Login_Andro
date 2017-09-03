0bdd568fa0218e4044a2e8f8dea17f74b5d44fad
killingrocks <niclaus.a@gmail.com>
2017-09-03 12:41:51 -0500

    initial commit

---
+++ app/src/main/java/com/example/meoryou/login/LogIn.java
@@ -0,0 +1,20 @@
+package com.example.meoryou.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+
+
+    public void logInOnClik(View view){}
+    public void forgetPasswordOnClick(View view){}
+    public void signUpOnClick(View view){}
+}
